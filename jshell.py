896a72b5e9e43c835f51bc1f449dbdecbe6bb7fc
joey m <34293717+joeyy-miller@users.noreply.github.com>
2023-04-15 23:54:07 -0700

    Changed Case System

--- jshell.py
+++ jshell.py
@@ -1,29 +1,37 @@
 import sys
 import re
 import os
+from datetime import date
 
 DIR = os.getcwd()
 VERSION = "041023a"
 
 def commands(input):
-    match input:
-        case "about":
-        case "help":
-            print("jshell version " + VERSION)
 
-        case "pwd":
-            print(DIR)
+    # Version
+    if input == "about":
+        print("jshell version " + VERSION)
 
-        case "ls":
-            print("ls")
-            dir_list = os.listdir(os.getcwd()) 
-            # prints all files
-            print(dir_list)
+    if input == "help":
+        print("commands: about, help, pwd, ls, cd, cd ..")
+
+    if input == "pwd":
+        print(DIR)
+
+    if input == "ls":
+        print("ls")
+        dir_list = os.listdir(os.getcwd()) 
+        # prints all files
+        print(dir_list)
+
+    if input == "cd .."
+        os.chdir("..")
+        DIR = os.path.abspath(os.curdir)
+        print ("Moved up to: " + DIR)
     
-        case "cd ..":
-            os.chdir("..")
-            DIR = os.path.abspath(os.curdir)
-            print ("Moved up to: " + DIR)
+    if input == "date":
+        today = date.today()
+        print(today)
 
     if (re.match("cd(.*)", input)):
         print("you cd'd")
