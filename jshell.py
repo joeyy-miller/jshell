d94cc51afce9069d3415a728fd1871d329831a14
joey m <34293717+joeyy-miller@users.noreply.github.com>
2023-04-17 00:34:13 -0700

    Added 'delete' command, reformatted 'ls' command

--- jshell.py
+++ jshell.py
@@ -4,53 +4,81 @@ import os
 from datetime import date
 from datetime import datetime
 
-DIR = ""
-VERSION = "041523d"
+VERSION = "0230417e"
+DIRECTORY = ""
 
 def commands(input):
 
+    global DIRECTORY
     # Version
     if input == "about":
-        print("jshell version " + VERSION)
-
-    if input == "help":
-        print("commands: about, help, pwd, ls, cd, date, time")
-
-    if input == "pwd":
-        print(DIR)
-
-    if input == "ls":
-        print("ls")
-        dir_list = os.listdir(os.getcwd()) 
-        # prints all files
-        print(dir_list)
+        jmsg("jshell version " + VERSION)
     
-    if input == "date":
-        today = date.today()
-        print(today)
-    
-    if input == "time":
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print("Current Time =", current_time)
-
-    if (re.match("cd (.*)", input)):
+    elif (re.match("cd (.*)", input)):
         if input == "cd ..":
             os.chdir("..")
-            DIR = os.path.abspath(os.curdir)
-            print ("Moved up to: " + DIR)
+            DIRECTORY = os.path.abspath(os.curdir)
+            jmsg("Moved up to: " + DIRECTORY)
         else:
             directory = input[3:]
             try:
                 os.chdir(directory)
             except FileNotFoundError:
-                print("Not a valid directory: " + directory)
+                print_err("not a valid directory: " + directory)
             except:
-                 print("Unkown: " + directory)
+                print_err("Unkown: " + directory)
+        
+    elif input == "date":
+        today = date.today()
+        jmsg(today)
+
+    elif (re.match("del (.*)", input)):
+        remove_file = input[4:]
+        jmsg("deleting file" + remove_file)
+        try:
+            os.remove(remove_file)
+        except FileNotFoundError:
+            print_err("not a valid file: " + remove_file)
+        except:
+            print_err("Unkown: " + remove_file)
+    
+    elif input == "exit":
+        sys.exit()
+    
+
+    elif input == "help":
+        jmsg("commands: about, help, pwd, ls, cd, date, time, del, exit")
+
+    elif input == "ls":
+        dir_list = os.listdir(os.getcwd()) 
+        # prints all files
+        print(">", end=" ")
+        for directory in dir_list:
+            print(directory + "  ", end=" ")
+        print(" ")
+
+    elif input == "pwd":
+        jmsg(DIRECTORY)
+    
+    elif input == "time":
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        jmsg("Current Time =", current_time)
+
+    else:
+        print_err("command not found: " + input)
+    
+
+def print_err(err):
+    print(">jsh error: " + err)
+
+def jmsg(msg):
+    print(">jsh: " + msg)
 
 def main():
     os.system("clear")
-    DIR = os.path.abspath(os.curdir)
+    global DIRECTORY
+    DIRECTORY = os.path.abspath(os.curdir)
     while True:
         print("$ ", end="")
         commands(input())
