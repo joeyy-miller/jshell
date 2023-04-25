da510bd61d5a4b7361b20ef91734cccb99aac151
joey m <34293717+joeyy-miller@users.noreply.github.com>
2023-04-25 16:07:57 -0700

    Added LS -l

--- jshell.py
+++ jshell.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import os
+import time
 from datetime import date
 from datetime import datetime
 
@@ -71,32 +72,57 @@ def commands(input):
         jmsg("commands: about, help, pwd, ls, cd, date, time, del, exit, mk, mkdir, make, rm, rmdir, echo")
 
     # LIST FILES ls
-    elif input == "ls":
+    elif (re.match("ls (.*)", input) or input == "ls"):
+        human_readable = False
+        if (re.match("ls -l(.*)", input)):
+            human_readable = True
+
         dir_list = os.listdir(os.getcwd()) 
         # prints all files
-        max_length = -1;
-        for jsh.directory in dir_list:
-            if len(jsh.directory) > max_length:
-                max_length = len(jsh.directory)
-        
-        line_len = 70 # Max items per line
-        count = 0 # Count the number of items printed
-        for jsh.directory in dir_list:
-            count += len(jsh.directory)
-            if count >= line_len:
-                print(" ")
-                count = 0
-            isFile = os.path.isfile(jsh.directory)
-            if isFile:
-                print(ANSI.color_text(35) + jsh.directory + ANSI.color_text(0), end=" ")
-            else:
-                print(ANSI.color_text(34) + jsh.directory + ANSI.color_text(0), end=" ")
-            # print(jsh.directory + "  ", end=" ")
-            for i in range(max_length - len(jsh.directory)):
-                print(" ", end="")
-                count += 1
+        if not human_readable:
+            max_length = -1;
+            for jsh.directory in dir_list:
+                if len(jsh.directory) > max_length:
+                    max_length = len(jsh.directory)
             
-        print(" ")
+            line_len = 70 # Max items per line
+            count = 0 # Count the number of items printed
+            for jsh.directory in dir_list:
+                count += len(jsh.directory)
+                if count >= line_len:
+                    print(" ")
+                    count = 0
+                isFile = os.path.isfile(jsh.directory)
+                if isFile:
+                    print(ANSI.color_text(35) + jsh.directory + ANSI.color_text(0), end=" ")
+                else:
+                    print(ANSI.color_text(34) + jsh.directory + ANSI.color_text(0), end=" ")
+                # print(jsh.directory + "  ", end=" ")
+                for i in range(max_length - len(jsh.directory)):
+                    print(" ", end="")
+                    count += 1
+            print(" ")
+        else:
+            max_length = -1;
+            count = 0
+            for jsh.directory in dir_list:
+                if len(jsh.directory) > max_length:
+                    max_length = len(jsh.directory)
+                print(" ", end="")
+
+            for jsh.directory in dir_list:
+                # Give us the current file
+                file_to_read = jsh.directory
+
+                # Get the file modifed date
+                file_modifed = str(time.ctime(os.path.getmtime(file_to_read)))
+
+                # Get the file's permissions
+                st = os.stat(file_to_read)
+                oct_perm = oct(st.st_mode)
+                mask = oct(os.stat(file_to_read).st_mode)[-3:] # convert the permissions 
+
+                print("r:" + mask + "m:" + file_modifed + "f:" + jsh.directory)
 
     # MAKE FILE make or mk
     elif (re.match("make (.*)", input) or re.match("mk (.*)", input)):
@@ -131,7 +157,7 @@ def commands(input):
     elif input == "time":
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
-        jmsg("Current Time =", current_time)
+        jmsg("Current Time: " + current_time)
 
     # UNKOWN COMMAND
     else:
