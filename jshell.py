2df30ad9bcf54e03cc3677524540fd5ca5dc695d
joey m <34293717+joeyy-miller@users.noreply.github.com>
2023-05-01 22:41:53 -0700

    Minor Formatting

--- jshell.py
+++ jshell.py
@@ -18,10 +18,8 @@ class jshell:
 class ANSI():
     def background(code):
         return "\33[{code}m".format(code=code)
- 
     def style_text(code):
         return "\33[{code}m".format(code=code)
- 
     def color_text(code):
         return "\33[{code}m".format(code=code)
 
