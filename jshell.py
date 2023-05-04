80fbe83530eef649ad2cf24826f959e091e003ca
joey m <34293717+joeyy-miller@users.noreply.github.com>
2023-05-03 21:10:01 -0700

    Added key

--- jshell.py
+++ jshell.py
@@ -14,6 +14,7 @@ class jshell:
         self.directory = ""
         self.userkey = jsettings['UserString']
         self.output = ""
+        self.key = ""
         
 class ANSI():
     def background(code):
